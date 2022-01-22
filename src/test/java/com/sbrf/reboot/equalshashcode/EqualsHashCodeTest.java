e19a6ff35f6559fbb4735274dac18a9684ed7832
Тедеев Тимур <tedeev_tp@interrao.ru>
2022-01-22 20:08:36 +0300

    HomeWork Lesson - 7

--- src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
+++ src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class EqualsHashCodeTest {
 
-     class Car {
+    class Car {
         String model;
         String color;
         Calendar releaseDate;
@@ -16,19 +17,38 @@ public class EqualsHashCodeTest {
 
         @Override
         public boolean equals(Object o) {
-
-            //Рефлексивность: объект должен равняться самому себе
-            if (o == this)
+            //В первую проверяем ссылки сравниваемых объектов и если они равны, то сразу возвращаем true
+            if (o == this) {
                 return true;
-
-            return false;
+            }
+            //Проверим не является ли ссылка null
+            if (o == null) {
+                return false;
+            }
+            //Сравниваем типы объектов
+            if (o.getClass() != getClass()) {
+                return false;
+            }
+            //Преобразуем тип параметра к нашему классу (Car)
+            Car car = (Car) o;
+            //В этом выражении мы сравниваем все значимые поля объектов и если хоть одно поле не равно, вернется false
+            return Objects.equals(model, car.model) &&
+                    Objects.equals(color, car.color) &&
+                    Objects.equals(releaseDate, car.releaseDate) &&
+                    maxSpeed == car.maxSpeed;
         }
 
-
-     }
+        @Override
+        public int hashCode() {
+            return model.hashCode() *
+                    color.hashCode() *
+                    releaseDate.hashCode() *
+                    maxSpeed;
+        }
+    }
 
     @Test
-    public  void assertTrueEquals() {
+    public void assertTrueEquals() {
         Car car1 = new Car();
         car1.model = "Mercedes";
         car1.color = "black";
@@ -63,7 +83,7 @@ public class EqualsHashCodeTest {
     }
 
     @Test
-    public void successEqualsHashCode(){
+    public void successEqualsHashCode() {
         Car car1 = new Car();
         car1.model = "Mercedes";
         car1.color = "black";
@@ -76,12 +96,12 @@ public class EqualsHashCodeTest {
         car2.releaseDate = new GregorianCalendar(2020, 0, 25);
         car2.maxSpeed = 10;
 
-        Assertions.assertEquals(car1.hashCode(),car2.hashCode());
+        Assertions.assertEquals(car1.hashCode(), car2.hashCode());
 
     }
 
     @Test
-    public void failEqualsHashCode(){
+    public void failEqualsHashCode() {
         Car car1 = new Car();
         car1.model = "Mercedes";
         car1.color = "black";
@@ -94,9 +114,74 @@ public class EqualsHashCodeTest {
         car2.releaseDate = new GregorianCalendar(2017, 0, 25);
         car2.maxSpeed = 10;
 
-        Assertions.assertNotEquals(car1.hashCode(),car2.hashCode());
+        Assertions.assertNotEquals(car1.hashCode(), car2.hashCode());
 
     }
 
+    class Sportsman {
+        int age;
+        int weight;
+        String name;
+        String sportName;
 
+        public Sportsman(String name, int age, int weight, String sportName) {
+            this.age = age;
+            this.weight = weight;
+            this.name = name;
+            this.sportName = sportName;
+        }
+
+        /* Мой пример
+         *
+         * В этом примере я переопределил метод equals таким орбразом, что бы сравнивать только по полям:
+         * age, weight
+         * У нас проходят соревнования и надо подобрать пары для схваток по: весу и возрасту.
+         * Необходимо проверять кто подходит по возрасту и весовой категории
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Sportsman sportsman = (Sportsman) o;
+            return age == sportsman.age &&
+                    Objects.equals(weight, sportsman.weight);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, weight);
+        }
+    }
+
+    @Test
+    public void successEqualsHashCodeSportsmens() {
+        Sportsman sportsman1 = new Sportsman("Григорий", 18, 50, "Вольная Борьба");
+        Sportsman sportsman2 = new Sportsman("Олег", 18, 50, "Вольная Борьба");
+
+        Assertions.assertTrue(sportsman1.equals(sportsman2));
+    }
+
+    @Test
+    public void failEqualsHashCodeSportsmens() {
+        Sportsman sportsman1 = new Sportsman("Александр", 21, 50, "Вольная Борьба");
+        Sportsman sportsman2 = new Sportsman("Юрий", 30, 84, "Вольная Борьба");
+
+        Assertions.assertFalse(sportsman1.equals(sportsman2));
+    }
+
+    @Test
+    public void successEqualsSportsmens() {
+        Sportsman sportsman1 = new Sportsman("Григорий", 18, 50, "Вольная Борьба");
+        Sportsman sportsman2 = new Sportsman("Олег", 18, 50, "Вольная Борьба");
+
+        Assertions.assertEquals(sportsman1.hashCode(), sportsman2.hashCode());
+    }
+
+    @Test
+    public void failEqualsSportsmens() {
+        Sportsman sportsman1 = new Sportsman("Александр", 21, 50, "Вольная Борьба");
+        Sportsman sportsman2 = new Sportsman("Юрий", 30, 84, "Вольная Борьба");
+
+        Assertions.assertNotEquals(sportsman1.hashCode(), sportsman2.hashCode());
+    }
 }
