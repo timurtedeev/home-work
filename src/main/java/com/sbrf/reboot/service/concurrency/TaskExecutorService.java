3920a747244bde17064bab0727df0c4b3363edad
Timur Tedeev <timka.tedeev@yandex.ru>
2022-03-04 22:36:10 +0300

    HomeWork Lesson - 13

--- src/main/java/com/sbrf/reboot/service/concurrency/TaskExecutorService.java
+++ src/main/java/com/sbrf/reboot/service/concurrency/TaskExecutorService.java
@@ -15,7 +15,9 @@ public class TaskExecutorService {
     }
 
     public void execute(Task task) {
-        service.execute(task);
+        for (int i = 0; i < this.numberOfThreads; i++) {
+            service.execute(task);
+        }
     }
 
     public void shutdown() {
